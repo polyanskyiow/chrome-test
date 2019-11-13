a95661f913badb0ec5f794e18ae8ba0e3efba6e5
polyanskyiow <polyanskyiow>
2019-11-13 19:58:56 +0200

    init

---
+++ src/test/java/ChromeTest.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ChromeTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setupTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://google.com");
+    }
+
+}
